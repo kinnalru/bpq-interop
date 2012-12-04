92f81924d5bedf20b7dcd0ebdc8c348fd304527a
Samoilenko Yuri <kinnalru@gmail.com>
2012-12-04 16:55:08 +0400

    work

--- main.cpp
+++ main.cpp
@@ -9,17 +9,25 @@
 
 #include <QApplication>
 #include <QTimer>
+#include <QLineEdit>
 
 #include "test_signal.h"
 
 using namespace std;
 
 
+using namespace boost;
+using namespace boost::python;
+using namespace boost::python::converter;
 namespace py = boost::python;
 
 
 qint64 unwrap(const QObject& ptr) {
     return reinterpret_cast<qint64>(&ptr);
+}
+
+QObject& wrap(qint64 pyptr) {
+    return reinterpret_cast<QObject&>(pyptr);
 }
 
 /** to-python convert to QObjects */
@@ -31,10 +39,91 @@ struct QObject_to_python
     }
 };
 
+struct QWidget_from_python
+{
+	QWidget_from_python()
+	{
+		boost::python::converter::registry::push_back(
+		&convertible,
+		&construct,
+		boost::python::type_id<QWidget*>());
+
+		boost::python::converter::registry::push_back(
+		&convertible,
+		&construct,
+		boost::python::type_id<QWidget>());
+
+		boost::python::converter::registry::push_back(
+		&convertible,
+		&construct,
+		boost::python::type_id<QWidget&>());
+
+		boost::python::converter::registry::push_back(
+		&convertible,
+		&construct,
+		boost::python::type_id<const QWidget&>());
+	}
+
+	static void* convertible(PyObject* obj_ptr)
+	{
+	//       if (!PyString_Check(obj_ptr)) return 0;
+// 		return obj_ptr;
+
+		std::cerr <<"HERE1" << std::endl;
+		std::cerr << obj_ptr->ob_type->tp_doc << std::endl;
+
+		return 0;
+	}
+
+	static void construct(
+		PyObject* obj_ptr,
+		boost::python::converter::rvalue_from_python_stage1_data* data)
+	{
+		std::cerr << "HERE" << std::endl;
+// 		const char* value = PyString_AsString(obj_ptr);
+// 		
+// 		if (value == 0) boost::python::throw_error_already_set();
+// 		void* storage = (
+// 		(boost::python::converter::rvalue_from_python_storage<custom_string>*)
+// 			data)->storage.bytes;
+// 		new (storage) custom_string(value);
+// 		data->convertible = storage;
+	}
+};
+
+void* extract_pointer_1(PyObject* obj)
+{
+	std::cerr << "HERE" << std::endl;
+	std::cerr << obj->ob_type->tp_doc << std::endl;
+
+	try {
+		object o1(borrowed(obj));
+	
+		object o2 = import("sip").attr("unwrapinstance")(o1);
+	
+		std::cerr << "here2" << std::endl;
+		std::cerr << "Val:" << extract<quint64>(o2) << std::endl;
+		std::cerr << "here3" << std::endl;
+	}
+	catch(const std::exception& e) {
+		std::cerr << "Exception:" << e.what() << std::endl;
+	}
+	
+// 		return incref(obj.ptr());
+// 		
+// 	if ()
+	
+	return 0;
+}
+
+
+
 void initializeConverters()
 {
     // register the to-python converter
     boost::python::to_python_converter<QObject, QObject_to_python>();
+
+	QWidget_from_python();
 }
 
 int main(int argc, char* argv[])
@@ -44,6 +133,8 @@ int main(int argc, char* argv[])
     QApplication app(argc, argv);
     
     Py_Initialize();
+
+	converter::registry::insert(&extract_pointer_1, type_id<QWidget>());
     
     py::object main_module = py::import("__main__");
     py::object main_namespace = main_module.attr("__dict__");
@@ -53,17 +144,41 @@ int main(int argc, char* argv[])
     
     TestSignal ts;
     ts.connect(&ts, SIGNAL(activate(QString)), SLOT(print(QString)));
-    
+
+// 	QLineEdit* le = new QLineEdit();
+
+// 	QObject* o = main_namespace["w"].extract<QObject*>();
+
     try
     {
-        py::object ptr(static_cast<QObject&>(ts));
-        main_namespace["ts_raw"] = ptr;
+        main_namespace["ts_raw"] = static_cast<QObject&>(ts);
+		main_namespace["www"] = 1;
+		main_namespace["iww"] = 1;
+
+	
         boost::python::exec_file("./main.py", main_namespace, main_namespace);
-//         return app.exec();
+
+		std::cerr << "ok cerr" << std::endl;
+		std::cout << "ok cout" << std::endl;
+// 
+// 		py::object o = main_namespace["w"];
+		
+// 		std::cerr << get_lvalue_from_python(o.ptr(), registered_pointee<QWidget>::converters) << std::endl;;
+
+		try{
+			QWidget* o = py::extract<QWidget*>(main_namespace["www"]);
+		}
+		catch(...) {
+			PyErr_Print();
+			std::cerr <<"can't convert" << std::endl;
+		}
+		
+        return app.exec();
     }
     catch(py::error_already_set const &)
     {
         PyErr_Print();
+		return -1;
     }   
 
     
--- main.py
+++ main.py
@@ -7,34 +7,38 @@ from PyQt4 import QtCore
 
 #wrap raw pointer to specified Qt-class
 def wrap(type, ptr):
-    return sip.wrapinstance(ptr, type)
+	return sip.wrapinstance(ptr, type)
 
-def main():
+#def main():
 
-    # ts_raw - loaded from C++ environment
+	# ts_raw - loaded from C++ environment
 
-    print("raw value:", ts_raw);
-    ts_wrapped = wrap(QtCore.QObject, ts_raw);
-    ts_wrapped.trigger_str("hello from python!")
-    
-    app = QtCore.QCoreApplication.instance()
-    if not (app): app = QtGui.QApplication(sys.argv)
+print("raw value:", ts_raw);
+ts_wrapped = wrap(QtCore.QObject, ts_raw);
+ts_wrapped.trigger_str("hello from python!")
 
-    w = QtGui.QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    
-    btn = QtGui.QPushButton('Button', w)
-    btn.setToolTip('This is a <b>QPushButton</b> widget')
-    btn.resize(btn.sizeHint())
-    btn.move(50, 50)      
-    btn.clicked.connect(ts_wrapped.trigger)
-    
-    w.show()
-    
-    sys.exit(app.exec_())
+app = QtCore.QCoreApplication.instance()
+if not (app): app = QtGui.QApplication(sys.argv)
 
 
-if __name__ == '__main__':
-    main()
+print("raw www:", www);
+www = QtGui.QWidget()
+print("raw2 www:", www);
+www.resize(250, 150)
+www.move(300, 300)
+www.setWindowTitle('Simple')
+
+btn = QtGui.QPushButton('Button', www)
+btn.setToolTip('This is a <b>QPushButton</b> widget')
+btn.resize(btn.sizeHint())
+btn.move(50, 50)
+btn.clicked.connect(ts_wrapped.trigger)
+
+www.show()
+
+
+#sys.exit(app.exec_())
+
+
+#if __name__ == '__main__':
+	#main()
